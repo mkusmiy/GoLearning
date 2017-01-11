19adc4c56737d41a0f93f29d2d372da7652ba3a5
Michael Kusmiy <mkusmiy@gmail.com>
2017-01-11 20:45:27 +0200

    added CustomError exercise

---
+++ exercises/25_CustomError/CustomError.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	msg := fmt.Sprint("Cannot Sqrt negative number: ", float64(e))
+	return msg
+}
+
+func NSqrt(x float64) (float64, error) {
+	z := 1.0
+	if x == 0 {
+		z = 0
+	} else if x > 0 {
+		for i := 0; i < 10; i++ {
+			z = 0.5 * (z + x/z)
+		}
+	} else {
+		return 0, errors.New(ErrNegativeSqrt(x).Error())
+	}
+	return z, nil
+}
+
+func main() {
+	var n float64 = -2
+	fmt.Println(NSqrt(n))
+	fmt.Println(math.Sqrt(n))
+
+}
---
+++ exercises/25_CustomError/CustomError_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+// Test runs a test suite against WordCount.
+func Test(t *testing.T) {
+	ok := true
+	for _, c := range testCases {
+		got, _ := NSqrt(c.in)
+		if c.want != got {
+			ok = false
+		}
+		if !ok {
+			t.Fatalf("FAIL\n NSqrt(%v) = %#v\n want:  %#v",
+				c.in, got, c.want)
+			break
+		}
+		t.Logf("PASS\n NSqrt(%v) = %#v\n", c.in, got)
+	}
+}
+
+var testCases = []struct {
+	in   float64
+	want float64
+}{
+	{4, 2},
+	{9, 3},
+	{0, 0},
+	{-1, 0},
+}
